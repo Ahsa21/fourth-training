4841a85e740377653c68adb6f7a282a2a02b8d44
Ahsa21 <ahmadsalouka@gmail.com>
2024-12-24 10:34:24 -0800

    Initial Commit

---
+++ fourth training/fourth_trainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  fourth_trainingApp.swift
+//  fourth training
+//
+//  Created by Ahmad Saloukha on 2024-12-24.
+//
+
+import SwiftUI
+
+@main
+struct fourth_trainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ fourth trainingUITests/fourth_trainingUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  fourth_trainingUITestsLaunchTests.swift
+//  fourth trainingUITests
+//
+//  Created by Ahmad Saloukha on 2024-12-24.
+//
+
+import XCTest
+
+final class fourth_trainingUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
